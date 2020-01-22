8867aa8a1247ec67bcb84eee14cedb85b14ea829
XabierGallardo <javiergallard91@gmail.com>
2020-01-22 23:59:20 +0000

    Created file and initial exercices

---
+++ exercices.js
@@ -0,0 +1,19 @@
+//Return x doubled
+function double (x) {
+	return x * 2;
+}
+
+//x is a number, return x as a string
+function numberToString(x) {
+	return x + "";
+}
+
+//x is a float, return x as a truncated int
+function floatToInt(x) {
+	return Math.trunc(x);
+}
+
+//return x squared 
+function square(x) {
+	return x * x;
+}
