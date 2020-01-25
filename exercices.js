e9ffdfa0e51e90dbf95e52092f943e5ff87da63e
XabierGallardo <javiergallard91@gmail.com>
2020-01-25 23:59:39 +0000

    Added new exercices and fixed indentation

--- exercices.js
+++ exercices.js
@@ -3,20 +3,32 @@ function double (x) {
 	return x * 2;
 }
 
+
+
+
 //2. x is a number, return x as a string
 function numberToString(x) {
 	return x + "";
 }
+
+
+
 
 //3. x is a float, return x as a truncated int
 function floatToInt(x) {
 	return Math.trunc(x);
 }
 
+
+
+
 //4. return x squared 
 function square(x) {
 	return x * x;
 }
+
+
+
 
 //5. x is an integer, return false if the number is odd and true if it's even
 function isEven(x) {
@@ -27,6 +39,9 @@ function isEven(x) {
 	}
 }
 
+
+
+
 //6. x is an array, return an array removing the first 5 elements on x
 function removeFirstFive(x) {
 	for (let i =0; i < 5; i++) {
@@ -35,11 +50,17 @@ function removeFirstFive(x) {
 	return x;
 }
 
+
+
+
 //7. x is a string that contains a number, return x as a number
 function stringToNumber(x) {
 	let integer = parseFloat(x);
 	return integer;
 }
+
+
+
 
 //8. x is a string, rearrange the string from the last to the first character
 function reverseString(x) {
@@ -49,6 +70,9 @@ function reverseString(x) {
 	return joinArray;
 }
 
+
+
+
 //9. x is an array, return an array removing the last 5 elements on x
 function removeLastFive(x) {
 	function removeFirstFive(x) {
@@ -57,6 +81,9 @@ function removeLastFive(x) {
         }
         return x;
 }
+
+
+
 
 //10. sum the digits of the number x
 function sumDigits(x) {
@@ -82,6 +109,8 @@ myArray = [...myArray,"end"];
 console.log(myArray);
 
 
+
+
 //25. How do you create a private variable in JavaScript?
 //Scope determines the accessibility (visibility) of variables: global scope (outside of functions) and local scope (inside of functions). Remember that in JS, objets and functions are also variables!
 
@@ -94,6 +123,8 @@ function secretVariable() {
 
 var getPrivateVariable = secretVariable();
 console.log(getPrivateVariable()); //super secret code
+
+
 
 
 //26. What's the output?
@@ -110,6 +141,8 @@ function outer() {
 outer();
 
 
+
+
 //27. What's the output?
 console.log(typeof typeof 1);
 
@@ -118,3 +151,25 @@ console.log(typeof typeof 1);
 
 //	"string"
 
+
+
+
+//28. What's the output?
+var sum = 0;
+for (var i = 4; i < 8; i++) {
+	if ( i == 6) {
+		continue;
+	}
+	sum += i;
+}
+console.log(sum); //	16
+
+
+
+
+//29. What's the output?
+var x = 0;
+while (x < 6) {
+	x++;
+}
+console.log(x);//	6
